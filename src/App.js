6b1ac186cdf182a6e8ec8afbaa7785b7825f0f21
Asish-Tammana <tammanaasish@gmail.com>
2024-04-25 22:34:14 +0530

    slider changes

--- src/App.js
+++ src/App.js
@@ -4,131 +4,174 @@ import './App.css';
 
 function App() {
 
-  const [UTC, setUTC] = useState('12:00 AM');
-  const [IST, setIST] = useState('');
-  const [PT, setPT] = useState('');
-  const [EDT, setEDT] = useState('');
+  const [UTC, setUTC] = useState(['12:00 AM', 0]);
+  const [IST, setIST] = useState(['', 0]);
+  const [PT, setPT] = useState(['', 0]);
+  const [EDT, setEDT] = useState(['', 0]);
+  const [isInternalChange, setIsInternalChange] = useState(true);
+
+  const calculateAngle = (timeString) => {
+    const [hours, minutes] = timeString.split(':').map(Number);
+    const angle = (parseInt(((hours * 60) + minutes) * 0.25)) % 1440;
+    return angle;
+  }
 
   const convert12hrTo24hr = (timeString) => {
+
     const [timePart, ampm] = timeString.split(' ');
     const [hours, minutes] = timePart.split(':').map(Number);
-  
+
     const ampmUpperCase = ampm.toUpperCase();
-  
+
     let totalHours = hours;
-  
+
     if (ampmUpperCase === 'PM' && hours < 12) {
       totalHours += 12;
     } else if (ampmUpperCase === 'AM' && hours === 12) {
       totalHours = 0;
     }
-  
+
     const formattedTime = `${totalHours < 10 ? '0' + totalHours : totalHours}:${minutes < 10 ? '0' + minutes : minutes}`;
-  
+
     return formattedTime;
   }
-  
+
   const convert24hrTo12hr = (timeString) => {
     const [hours, minutes] = timeString.split(':').map(Number);
 
-    const minutes12Min = minutes%60
-  
+    const minutes12Min = minutes % 60
+
     const ampm = hours >= 12 ? 'PM' : 'AM';
-  
+
     let hours12hr = hours % 12;
     if (hours12hr === 0) {
-      hours12hr = 12; 
+      hours12hr = 12;
     }
-  
+
     const formattedTime = `${hours12hr < 10 ? '0' + hours12hr : hours12hr}:${minutes12Min < 10 ? '0' + minutes12Min : minutes12Min} ${ampm}`;
-  
+
     return formattedTime;
   }
-  
+
   const addHoursMinutesToTime = (timeString, hoursToAdd, minutesToAdd) => {
     const [hours, minutes] = timeString.split(':').map(Number);
-  
+
     let totalMinutes = minutes + minutesToAdd;
-  
+
     let totalHours = hours + hoursToAdd + Math.floor(totalMinutes / 60);
-  
+
     if (totalMinutes < 0) {
       totalMinutes += 60;
     }
-  
+
     if (totalHours < 0) {
       totalHours += 24;
     }
-  
+
     totalHours = totalHours % 24;
-  
+
     const formattedTime = `${totalHours < 10 ? '0' + totalHours : totalHours}:${totalMinutes < 10 ? '0' + totalMinutes : totalMinutes}`;
-  
+
     return formattedTime;
   }
 
-  const ISTChanged = (event) => {
-    setIST(event)
+  const get24hrsFromAngle = (givenAngle) => {
+    const hours = parseInt(givenAngle / 15)
+    const minutes = (givenAngle / 0.25) % 60
 
-    const format24 = convert12hrTo24hr(event)
+    return {hours, minutes}
+  }
+
+  const ISTChanged = (angle) => {
+    // const updatedAngle = calculateAngle(event)
+
+    const {hours, minutes} = get24hrsFromAngle(angle)
+    
+    const format24 = `${hours}:${minutes}`
+
+    setIST([convert24hrTo12hr(format24), angle])
+
     const utcFormat24 = addHoursMinutesToTime(format24, -5, -30)
+    const utcAngle = calculateAngle(utcFormat24)
     const utcFormat12 = convert24hrTo12hr(utcFormat24);
 
-    setTimeout(()=>{
-      setUTC(utcFormat12)
-    }, 100)
+    setTimeout(() => {
+      setUTC([utcFormat12,utcAngle])
+    }, 1000)
+    setIsInternalChange(true);
 
-    
 
   }
 
-  const UTCChanged = (event) => {
-    setUTC(event)
+  const PTChanged = (angle) => {
+
+    const {hours, minutes} = get24hrsFromAngle(angle)
+    
+    const format24 = `${hours}:${minutes}`
+    const utcFormat24 = addHoursMinutesToTime(format24, 7, 0)
+    const utcAngle = calculateAngle(utcFormat24)
+    const utcFormat12 = convert24hrTo12hr(utcFormat24);
+    
+    setTimeout(() => {
+      setUTC([utcFormat12, utcAngle])
+    }, 1000)
+    setIsInternalChange(true);
+
+  }
+
+  const EDTChanged = (angle) => {
+    // setEDT(event)
+    const {hours, minutes} = get24hrsFromAngle(angle)
+    
+    const format24 = `${hours}:${minutes}`
+    const utcFormat24 = addHoursMinutesToTime(format24, 4, 0)
+    const utcAngle = calculateAngle(utcFormat24)
+    const utcFormat12 = convert24hrTo12hr(utcFormat24);
+    setTimeout(() => {
+      setUTC([utcFormat12, utcAngle])
+    }, 1000)
+
+    setIsInternalChange(true);
+  }
+
+  const UTCChanged = (angle) => {
+    const {hours, minutes} = get24hrsFromAngle(angle)
+    const format24 = `${hours}:${minutes}`
+    setUTC([convert24hrTo12hr(format24), angle])
   }
 
   const changeRemainingTime = () => {
-    setIST(convert24hrTo12hr(addHoursMinutesToTime(convert12hrTo24hr(UTC), 5, 30)))
-    setPT(convert24hrTo12hr(addHoursMinutesToTime(convert12hrTo24hr(UTC), -7, 0)))
-    setEDT(convert24hrTo12hr(addHoursMinutesToTime(convert12hrTo24hr(UTC), -4, 0)))
+    
+    let updatedIST = addHoursMinutesToTime(convert12hrTo24hr(UTC[0]), 5, 30)
+    let updatedISTAngle = calculateAngle(updatedIST)
+    setIST([convert24hrTo12hr(updatedIST), updatedISTAngle])
+
+    let updatedPT = addHoursMinutesToTime(convert12hrTo24hr(UTC[0]), -7, 0)
+    let updatedPTAngle = calculateAngle(updatedPT)
+    setPT([convert24hrTo12hr(updatedPT), updatedPTAngle])
+
+    let updatedEDT = addHoursMinutesToTime(convert12hrTo24hr(UTC[0]), -4, 0)
+    let updatedEDTAngle = calculateAngle(updatedEDT)
+    setEDT([convert24hrTo12hr(updatedEDT), updatedEDTAngle])
+    setIsInternalChange(false);
+    
+
 
   }
 
-  const PTChanged = (event) => {
-    setPT(event)
-
-    const format24 = convert12hrTo24hr(event)
-    const utcFormat24 = addHoursMinutesToTime(format24, 7, 0)
-    const utcFormat12 = convert24hrTo12hr(utcFormat24);
-    setTimeout(()=>{
-      setUTC(utcFormat12)
-    },100)
-
-  }
-
-  const EDTChanged = (event) => {
-    setEDT(event)
-
-    const format24 = convert12hrTo24hr(event)
-    const utcFormat24 = addHoursMinutesToTime(format24, 4, 0)
-    const utcFormat12 = convert24hrTo12hr(utcFormat24);
-    setTimeout(()=>{
-      setUTC(utcFormat12)
-    }, 100)
-
-  }
-
-  useEffect(changeRemainingTime,[UTC])
+  useEffect(()=>{
+    if(isInternalChange){
+      changeRemainingTime();
+    }
+  } , [UTC])
+  
 
   return (
     <div className='time-box'>
-      {/* <span>UTC: </span> <br /><br /> */}
-      {/* <span>IST: </span> <input type='text' value={IST} onChange={ISTChanged} /><br /><br />
-      <span>PT: </span> <input type='text' value={PT} onChange={PTChanged}  /><br /><br />
-      <span>EDT: </span> <input type='text' value={EDT} onChange={EDTChanged}  /><br /><br /> */}
-      <TimeComp timezone={UTC} changeTime={UTCChanged} convert24hrTo12hr={convert24hrTo12hr} title="UTC" />
-      <TimeComp timezone={IST} changeTime={ISTChanged} convert24hrTo12hr={convert24hrTo12hr} title="IST" />
-      <TimeComp timezone={EDT} changeTime={EDTChanged} convert24hrTo12hr={convert24hrTo12hr} title="EDT" />
-      <TimeComp timezone={PT} changeTime={PTChanged} convert24hrTo12hr={convert24hrTo12hr} title="PT" />
+      <TimeComp timezone={UTC} changeTime={UTCChanged} title="UTC" />
+      <TimeComp timezone={IST} changeTime={ISTChanged} title="IST" />
+      <TimeComp timezone={EDT} changeTime={EDTChanged} title="EDT" />
+      <TimeComp timezone={PT} changeTime={PTChanged} title="PT" />
     </div>
   );
 }
--- src/components/TimeComp/index.js
+++ src/components/TimeComp/index.js
@@ -1,46 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import CircularSlider from '@fseehawer/react-circular-slider';
 import './index.css'
 
 const TimeComp = (props) => {
 
-    const {timezone, changeTime, convert24hrTo12hr, title} = props
-    const [angle, setAngle] = useState(0);
-    const [hours, setHours] = useState(0)
-    const [minutes, setMinutes] = useState(0);
-
-    const updateTime = () => {
-        setHours(parseInt(angle / 15))
-        const min = (angle / 0.25) % 60
-        setMinutes(min)
-    }
-
-    useEffect(() => {
-        // setAngle((parseInt(((hours * 60) + (minutes))*0.25)))
-        changeTime(convert24hrTo12hr(`${hours}:${minutes}`))
-    }, [hours, minutes])
-
-    useEffect(updateTime, [angle])
-
-
-   
+    const {timezone, changeTime, title} = props
 
     return (
         <div>
             <div className='time'>
                 <div>
                     <p>{title}</p>
-                    <h1 >{timezone}</h1>
+                    <h1 >{timezone[0]}</h1>
                 </div>
             </div>
             <CircularSlider
-                label=' IST Time'
                 width={200}
                 height={200}
                 valueFontSize='30px'
                 hideLabelValue={true}
-                dataIndex={angle}
-                onChange={(value) => setAngle(value)}
+                dataIndex={timezone[1]}
+                onChange={(value) => changeTime(value)}
                 className="slider"
             >
             </CircularSlider>
